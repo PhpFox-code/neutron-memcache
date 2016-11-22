128b033e13b026181915a6d9c07397bdd586ec2e
Nam Nguyen <nam.ngvan@gmail.com>
2016-11-22 16:33:18 +0700

    no message

---
+++ config/module.config.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Phpfox\Memcache;
+
+return [
+    'session.drivers' => [
+        'memcache'  => MemcacheSessionSaveHandler::class,
+        'memcached' => MemcachedSessionSaveHandler::class,
+    ],
+    'cache.drivers'   => [
+        'memcache'  => MemcacheCacheStorage::class,
+        'memcached' => MemcachedCacheStorage::class,
+    ],
+    'cache.adapters'  => [
+        'cache.memcached' => [
+            'driver'         => 'memcached',
+            'port'           => 11211,
+            'timeout'        => 1,
+            'persistent'     => true,
+            'retry_interval' => 15,
+            'servers'        => ['127.0.0.1'],
+        ],
+        'cache.memcache'  => [
+            'driver'         => 'memcache',
+            'port'           => 11211,
+            'timeout'        => 1,
+            'persistent'     => true,
+            'retry_interval' => 15,
+            'servers'        => ['127.0.0.1'],
+        ],
+    ],
+];
